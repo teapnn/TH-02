684479cd6ffe0e3d2fa03ab981ec256948b20a83
admin <admin@ADMIN-PC>
2018-03-16 17:43:11 +0700

    Add project files.

---
+++ LTM-02-Client/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Net.NetworkInformation;
+
+namespace LTM_02_Client
+{
+    public partial class Form1 : Form
+    {
+        Socket server, client;
+        IPEndPoint ipserver;
+        byte[] data;
+        string n;
+        byte[] b1;
+        OpenFileDialog op;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butSend_Click(object sender, EventArgs e)
+        {
+            string text = txtMessage.Text;
+            data = new byte[1024];
+            data = Encoding.ASCII.GetBytes(text);
+            txtMessage.Text = "";
+            listBox1.Items.Add(text);
+            client.Send(data);
+            data = new byte[1024];
+            client.Receive(data);
+            text = Encoding.ASCII.GetString(data);
+            listBox1.Items.Add(text);
+        }
+
+        private void butConnet_Click(object sender, EventArgs e)
+        {
+            ipserver = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 995);
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            client.Connect(ipserver);
+        }
+    }
+}
